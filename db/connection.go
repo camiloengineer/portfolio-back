13db794b018c5bec8b60358b8d2f2e7b37f07f97
Camilo González <cgonzalezm1234@gmail.com>
2023-10-12 11:10:44 -0300

    add secrets manager

--- db/connection.go
+++ db/connection.go
@@ -1,11 +1,15 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -18,18 +22,76 @@ func init() {
 	}
 }
 
-func DBConnection() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
+func getSecret(secretID string) (string, error) {
+	// projectID debe ser el ID de tu proyecto en GCP
+	projectID := os.Getenv("ID_PROJECT")
+	appEnv := os.Getenv("APP_ENV")
 
+	// Crea el contexto
+	ctx := context.Background()
+
+	var client *secretmanager.Client
+	var err error
+
+	if appEnv == "production" {
+		// En producción, usa la autenticación automática de App Engine
+		client, err = secretmanager.NewClient(ctx)
+	} else {
+		// En desarrollo, usa un archivo de credenciales
+		client, err = secretmanager.NewClient(ctx, option.WithCredentialsFile("credentials.json"))
+	}
+
+	if err != nil {
+		return "", fmt.Errorf("failed to create secretmanager client: %v", err)
+	}
+	defer client.Close()
+
+	// Construye el nombre del recurso del secreto
+	name := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectID, secretID)
+
+	// Solicita el secreto al Secret Manager API
+	result, err := client.AccessSecretVersion(ctx, &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to access secret version: %v", err)
+	}
+
+	// Devuelve el payload del secreto como string
+	return string(result.Payload.Data), nil
+}
+
+func DBConnection() {
+	// Obtén los secretos del Secret Manager
+	host, err := getSecret("DB_HOST")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := getSecret("DB_USER")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	password, err := getSecret("DB_PASSWORD")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbName, err := getSecret("DB_NAME")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port, err := getSecret("DB_PORT")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Construye la cadena de conexión y conecta a la base de datos
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, dbName, port)
 
-	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err)
 	} else {
--- go.mod
+++ go.mod
@@ -3,6 +3,16 @@ module github.com/camiloengineer/portfolio-back
 go 1.20
 
 require (
+	cloud.google.com/go/compute v1.19.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.1.0 // indirect
+	cloud.google.com/go/secretmanager v1.11.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -10,8 +20,19 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/api v0.126.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gorm.io/driver/postgres v1.5.2 // indirect
 	gorm.io/gorm v1.25.4 // indirect
 )
